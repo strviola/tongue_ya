dd3decbc600265c0f2e06b891814d89bc93a8201
strviola <the-brightest-moonlight.8107@mbp.nifty.com>
2016-11-06 10:43:04 +0900

    CSS debug

--- Gemfile
+++ Gemfile
@@ -1,7 +1,12 @@
 source "http://rubygems.org"
 
+ruby '2.3.1'
 gem 'sinatra'
 gem 'sass'
 gem 'haml'
 gem 'coffee-script'
 gem 'shotgun'
+
+gem 'pry-byebug'
+gem 'pry-doc'
+gem 'pry-stack_explorer'
--- main.rb
+++ main.rb
@@ -2,12 +2,17 @@ require 'sinatra/base'
 require 'haml'
 require 'sass'
 require 'coffee-script'
+require 'pry'
 
 class Server < Sinatra::Base
   get '/' do
     @text = "#{Time.now}"
     haml :index
   end
+
+  get '/stylesheets/main.css' do
+    sass :'assets/css/main'
+  end
 end
 
-run Server.run!
+Server.run!
